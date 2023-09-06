9e2fe05c7f630e520818c9ec7d7644b96ed974ea
Victor <victor.santos.cd@gmail.com>
2023-09-06 16:24:42 -0300

    fix sql creating_entities

--- sql/ddl/creating_entities.sql
+++ sql/ddl/creating_entities.sql
@@ -1,21 +1,30 @@
+SET TIMEZONE TO 'America/Sao_Paulo';
+
 CREATE TABLE IF NOT EXISTS company(
     id SERIAL PRIMARY KEY,
     name VARCHAR (100) UNIQUE NOT NULL,
-    summary TEXT NOT NULL,
-    "createdAt" DATETIME NOW,
-    "updatedAt" DATETIME NULL,
+    summary TEXT,
+    "createdAt" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ
 );
 
-CREATE TABLE IF NOT EXISTS type_of_snack(
-    id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS snack(
+    id SERIAL PRIMARY KEY, 
     name VARCHAR (100) UNIQUE NOT NULL,
     summary TEXT NOT NULL,
-    "createdAt" DATETIME NOW,
-    "updatedAt" DATETIME NULL,
+    id_company integer not null,
+    constraint fk_id_company FOREIGN KEY (id_company)
+    references company (id),
+    "createdAt" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ
 );
 
 CREATE TABLE IF NOT EXISTS nutricional_info(
     id SERIAL PRIMARY KEY,
+    snack_name VARCHAR (100), 
+    id_snack integer not null,
+    constraint fk_id_snack FOREIGN key (id_snack)
+    references snack (id),
     "Calories" integer NULL,
     "CaloriesFromFat" integer NULL,
     "TotalFat(g)" double precision NULL,
@@ -27,6 +36,6 @@ CREATE TABLE IF NOT EXISTS nutricional_info(
     "Fiber(g)" double precision NULL,
     "Sugars(g)" double precision NULL,
     "Protein(g)" double precision NULL,
-    "createdAt" DATETIME NULL,
-    "updatedAt" DATETIME NULL
+    "createdAt" TIMESTAMPTZ NULL,
+    "updatedAt" TIMESTAMPTZ NULL
 );
